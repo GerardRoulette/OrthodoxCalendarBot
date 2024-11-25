51b48723b5d405d850a2b8fcfdcd934af1c7b89e
GerardRoulette <evgeniy.kvasov@gmail.com>
2024-11-25 23:20:17 +0300

    save 2

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 const { Bot } = require('grammy');
 const schedule = require("node-schedule");
+const fs = require('fs');
+const path = require('path');
 
 const bot = new Bot(process.env.BOT_API_KEY);
-const chats = path.join(__dirname, 'chats.json'); // файл с контактами
+const chatsList = path.join(__dirname, 'chats.json'); // файл с контактами
+
+// ДОСТАЕМ СПИСОК КОНТАКТОВ ИЗ ФАЙЛА
+function ImportChats () {
+    if (fs.existsSync(chats)) {
+        return JSON.parse(fs.readFileSync(chats, 'utf8'));
+    }
+    return [];
+}
 
 bot.command('start', async (ctx) => {
 
