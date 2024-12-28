feb6410e52742d9a2b5979d8ef93d328d07b8117
GerardRoulette <evgeniy.kvasov@gmail.com>
2024-12-28 11:57:13 +0300

    save

--- functions/sendMessage.js
+++ functions/sendMessage.js
@@ -11,68 +11,65 @@ const scheduleFile = './schedule.json'
 
 // Отмена одного конкретного расписания по чат айди
 function cancelSchedule(chatId) {
-    if (scheduleMap.has(chatId)) {
-        scheduleMap.get(chatId).cancel();
-        scheduleMap.delete(chatId);
-        saveSchedules();
-    }
+  if (scheduleMap.has(chatId)) {
+    scheduleMap.get(chatId).cancel();
+    scheduleMap.delete(chatId);
+    saveSchedules();
+  }
 }
 
 // запись Мапа с расписаниями в JSON
 function saveSchedules() {
-    const schedules = Array.from(scheduleMap.entries()).map(([chatId, job]) => ({
-        chatId,
-        nextInvocation: job.nextInvocation().toISOString(),
-    }));
-    fs.writeFileSync(scheduleFile, JSON.stringify(schedules, null, 2));
+  const schedules = Array.from(scheduleMap.entries()).map(([chatId, job]) => ({
+    chatId,
+    nextInvocation: job.nextInvocation().toISOString(),
+  }));
+  fs.writeFileSync(scheduleFile, JSON.stringify(schedules, null, 2));
 }
 
 // пересоздание всех расписаний
 function restoreSchedules() {
-    if (fs.existsSync(scheduleFile)) {
-        const schedules = JSON.parse(fs.readFileSync(scheduleFile, 'utf-8'));
-        schedules.forEach(({ chatId, nextInvocation }) => {
-            const jobTime = new Date(nextInvocation);
-            if (jobTime > new Date()) {
-                scheduleMessage(chatId, ...getUserDetails(chatId)); // Fetch user details from DB
-            }
-        });
-    }
+  if (fs.existsSync(scheduleFile)) {
+    const schedules = JSON.parse(fs.readFileSync(scheduleFile, 'utf-8'));
+    schedules.forEach(({ chatId, nextInvocation }) => {
+      const jobTime = new Date(nextInvocation);
+      if (jobTime > new Date()) {
+        scheduleMessage(chatId, ...getUserDetails(chatId)); // Fetch user details from DB
+      }
+    });
+  }
 }
 
 
 function scheduleMessage(chatId, timezone, preferredTime) {
-    const [hour, minute] = preferredTime.split(':').map(Number);
-    
-    // вычисляем preferredtime в utc+0
-  const utcPreferredTime = new Date();
-  utcPreferredTime.setUTCHours(hour - timezone, minute, 0, 0);
+  const [hour, minute] = preferredTime.split(':').map(Number);
+  const localHour = (hour - timezone + 3 + 24) % 24;
+  // 3 - таймзона сервера. +24 чтобы число точно было положительное. % 24 чтобы убрать лишние часы
+  // 2:00 - 5 (utc+5) + 3 
+  // то есть 2 часа локал времени юзера - 5 таймзоны + 3 сервера = 0
+  // т.е. 2 часа в UTC+5 будут полночью по времени сервера
+  // +24 % 24 в случае если негативное число получилось 
+  const cronExpression = `${minute} ${localHour} * * *`;
 
-  // конвертируем во время сервера (+3 Москва)
-  const serverTime = new Date(utcPreferredTime);
-  serverTime.setHours(serverTime.getHours() + 3); // Convert from UTC+0 to server local time
 
-  // если время уже прошло, ставим на следующий
-  if (serverTime < new Date()) {
-    serverTime.setDate(serverTime.getDate() + 1);
-  }
+  // отмена существующего расписания если оно есть в мапе (если нет ничего не случится, ошибки не будет)
+  cancelSchedule(chatId);
 
-  
-    // отмена существующего расписания если оно есть в мапе (если нет ничего не случится, ошибки не будет)
-    cancelSchedule(chatId);
-  
-    const job = schedule.scheduleJob(serverTime, async () => {
+  const job = schedule.scheduleJob(cronExpression, async () => {
     // тут идея в том чтобы юзер получал календарь именно той даты которая ему нужна
-      const userDate = new Date(new Date().toLocaleDateString('en-CA', { timeZone: `UTC${timezone >= 0 ? '+' : ''}${timezone}` }));
-      const messageData = await getMessageByDate(userDate.toISOString().split('T')[0]);
-      if (messageData) {
-        bot.telegram.sendMessage(chatId, messageData.message, { parse_mode: 'HTML' });
-      }
-    });
-  
-    scheduleMap.set(chatId, job);
-    saveSchedules();
-  }
+    const userDate = new Date(new Date().toLocaleDateString('en-CA', { timeZone: `UTC${timezone >= 0 ? '+' : ''}${timezone}` }));
+    const messageData = await getMessageByDate(userDate.toISOString().split('T')[0]);
+
+    if (messageData) {
+      bot.api.sendMessage(chatId, messageData.message, { parse_mode: 'HTML' });
+    }
+  });
+
+  // Store the job for later cancellation or reference
+  scheduleMap.set(chatId, job);
+
+  saveSchedules();
+}
 
 
 
@@ -81,8 +78,8 @@ function scheduleMessage(chatId, timezone, preferredTime) {
 // bot.start();
 
 module.exports = {
-    restoreSchedules,
-    scheduleMessage,
-    cancelSchedule,
-    saveSchedules
+  restoreSchedules,
+  scheduleMessage,
+  cancelSchedule,
+  saveSchedules
 }
